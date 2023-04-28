067e0a12fb4a45750c29b9a2cae2c9bba1db6e6a
Carrington <Manyuchi>
2023-04-28 11:16:21 +0200

    Created the LoginVC and its UI Layout

---
+++ smack-app/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  smack-app
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/04/28.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
---
+++ smack-app/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack-app
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/04/28.
+//
+
+import Foundation
+
+
+let TO_LOGIN = "toLogin"
+
